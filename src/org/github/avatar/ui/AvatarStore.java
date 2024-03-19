f36f1d57ef8fef02f26c3587d604bf08e639ef80
András Péteri <apeteri@b2international.com>
2024-03-19 21:39:13 +0100

    feat: Update AvatarStore in accordance with documentation
    - Change hash algorithm to SHA256 from MD5
    - Remove "www" prefix from service URL
    - Change the expected number of hex digits to 64
    - Use String.format for padded BigInteger to String conversion
    - Use read and connection timeout (1000ms)
    - Use InputStream#transferTo method to pipe data to an output stream

--- src/org/github/avatar/ui/AvatarStore.java
+++ src/org/github/avatar/ui/AvatarStore.java
@@ -1,5 +1,7 @@
 /*******************************************************************************
  *  Copyright (c) 2011 Kevin Sawicki
+ *  Copyright 2024 B2i Healthcare, https://b2ihealthcare.com
+ *  
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -9,7 +11,6 @@
 package org.github.avatar.ui;
 
 import java.io.ByteArrayOutputStream;
-import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
@@ -20,24 +21,21 @@ import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.MessageFormat;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Pattern;
 
-import org.eclipse.core.runtime.Assert;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.*;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.Job;
 
 /**
  * Class that loads and stores avatars.
+ * <p>
+ * Relevant pages that document the service:
+ * <ul>
+ * <li><a href="https://docs.gravatar.com/general/hash/">https://docs.gravatar.com/general/hash/</a>
+ * <li><a href="https://docs.gravatar.com/general/images/">https://docs.gravatar.com/general/images/</a>
+ * </ul>
  * 
  * @author Kevin Sawicki (kevin@github.com)
  */
@@ -46,12 +44,14 @@ public class AvatarStore implements Serializable, ISchedulingRule, IAvatarStore 
 	/**
 	 * URL
 	 */
-	public static final String URL = "https://www.gravatar.com/avatar/"; //$NON-NLS-1$
+	// The previous URL included the "www" prefix and used HTTP for the protocol
+	public static final String URL = "https://gravatar.com/avatar/"; //$NON-NLS-1$
 
 	/**
 	 * HASH_REGEX
 	 */
-	public static final String HASH_REGEX = "[0-9a-f]{32}"; //$NON-NLS-1$
+	// Changed from MD5 to SHA256 which uses 64 hex digits
+	public static final String HASH_REGEX = "[0-9a-f]{64}"; //$NON-NLS-1$
 
 	/**
 	 * HASH_PATTERN
@@ -61,27 +61,31 @@ public class AvatarStore implements Serializable, ISchedulingRule, IAvatarStore 
 	/**
 	 * HASH_LENGTH
 	 */
-	public static final int HASH_LENGTH = 32;
+	// Changed hash length from 32 to 64
+	public static final int HASH_LENGTH = 64;
+
+	/**
+	 * Zero-padded hexadecimal string, using HASH_LENGTH as its width
+	 */
+	// Added to simplify hex string conversion
+	private static final String FORMAT_PATTERN = "%0" + HASH_LENGTH + "x";
 
 	/**
 	 * HASH_ALGORITHM
 	 */
-	public static final String HASH_ALGORITHM = "MD5"; //$NON-NLS-1$
+	// Changed algorithm from MD5 to SHA256
+	public static final String HASH_ALGORITHM = "SHA256"; //$NON-NLS-1$
 
 	/**
 	 * TIMEOUT
 	 */
-	public static final int TIMEOUT = 2 * 1000;
-
-	/**
-	 * BUFFER_SIZE
-	 */
-	public static final int BUFFER_SIZE = 8192;
+	// Changed timeout to 1 second from 2 seconds
+	public static final int TIMEOUT = 1000;
 
 	/**
 	 * serialVersionUID
 	 */
-	private static final long serialVersionUID = 6084425297832914970L;
+	private static final long serialVersionUID = 2L;
 
 	/**
 	 * Charset used for hashing
@@ -106,9 +110,12 @@ public class AvatarStore implements Serializable, ISchedulingRule, IAvatarStore 
 	 */
 	public AvatarStore(String url) {
 		Assert.isNotNull(url, "Url cannot be null"); //$NON-NLS-1$
+
 		// Ensure trailing slash
-		if (!url.endsWith("/")) //$NON-NLS-1$
+		if (!url.endsWith("/")) { //$NON-NLS-1$
 			url += "/"; //$NON-NLS-1$
+		}
+		
 		this.url = url;
 		this.avatars = Collections.synchronizedMap(new HashMap<String, Avatar>());
 	}
@@ -132,12 +139,13 @@ public class AvatarStore implements Serializable, ISchedulingRule, IAvatarStore 
 	 */
 	public IAvatarStore scheduleRefresh() {
 		Job refresh = new Job(Messages.AvatarStore_RefreshJobName) {
-
+			@Override
 			protected IStatus run(IProgressMonitor monitor) {
 				refresh(monitor);
 				return Status.OK_STATUS;
 			}
 		};
+		
 		refresh.setRule(this);
 		refresh.schedule();
 		return this;
@@ -150,20 +158,27 @@ public class AvatarStore implements Serializable, ISchedulingRule, IAvatarStore 
 		if (monitor == null) {
 			monitor = new NullProgressMonitor();
 		}
+		
 		String[] entries = null;
 		synchronized (this.avatars) {
 			entries = new String[this.avatars.size()];
 			entries = this.avatars.keySet().toArray(entries);
 		}
+		
 		monitor.beginTask("", entries.length); //$NON-NLS-1$
+		
 		for (String entry : entries) {
 			monitor.setTaskName(MessageFormat.format(Messages.AvatarStore_LoadingAvatar, entry));
+			
 			try {
 				loadAvatarByHash(entry);
 			} catch (IOException ignore) {
+				// It is not an issue if we can't fetch any of the avatars
 			}
+			
 			monitor.worked(1);
 		}
+		
 		monitor.done();
 		this.lastRefresh = System.currentTimeMillis();
 		return this;
@@ -180,33 +195,40 @@ public class AvatarStore implements Serializable, ISchedulingRule, IAvatarStore 
 	}
 
 	/**
-	 * @see org.github.avatar.ui.IAvatarStore#loadAvatarByHash(java.lang.String,
-	 *      org.github.avatar.ui.IAvatarCallback)
+	 * @see org.github.avatar.ui.IAvatarStore#loadAvatarByHash(java.lang.String, org.github.avatar.ui.IAvatarCallback)
 	 */
 	public IAvatarStore loadAvatarByHash(final String hash, final IAvatarCallback callback) {
 		String title = MessageFormat.format(Messages.AvatarStore_LoadingAvatar, hash);
 		Job job = new Job(title) {
-
+			@Override
 			protected IStatus run(IProgressMonitor monitor) {
 				try {
 					Avatar avatar = loadAvatarByHash(hash);
-					if (avatar != null && callback != null)
+					
+					// Notify interested parties that the avatar was fetched successfully
+					if (avatar != null && callback != null) {
 						callback.loaded(avatar);
+					}
+					
 				} catch (IOException e) {
-					if (callback != null)
+
+					// Notify interested parties that an error occurred while fetching the avatar
+					if (callback != null) {
 						callback.error(e);
+					}
 				}
+				
 				return Status.OK_STATUS;
 			}
 		};
+		
 		job.setRule(this);
 		job.schedule();
 		return this;
 	}
 
 	/**
-	 * @see org.github.avatar.ui.IAvatarStore#loadAvatarByEmail(java.lang.String,
-	 *      org.github.avatar.ui.IAvatarCallback)
+	 * @see org.github.avatar.ui.IAvatarStore#loadAvatarByEmail(java.lang.String, org.github.avatar.ui.IAvatarCallback)
 	 */
 	public IAvatarStore loadAvatarByEmail(String email, IAvatarCallback callback) {
 		loadAvatarByHash(getHash(email), callback);
@@ -217,38 +239,35 @@ public class AvatarStore implements Serializable, ISchedulingRule, IAvatarStore 
 	 * @see org.github.avatar.ui.IAvatarStore#loadAvatarByHash(java.lang.String)
 	 */
 	public Avatar loadAvatarByHash(String hash) throws IOException {
-		if (!isValidHash(hash))
+		if (!isValidHash(hash)) {
 			return null;
+		}
 
 		Avatar avatar = null;
 		HttpURLConnection connection = (HttpURLConnection) new URL(this.url + hash).openConnection();
+
+		// Set both connection and read timeouts (originally just connection timeout was defined)
+		connection.setReadTimeout(TIMEOUT);
 		connection.setConnectTimeout(TIMEOUT);
 		connection.setUseCaches(false);
 		connection.connect();
 
-		if (connection.getResponseCode() != 200)
+		if (connection.getResponseCode() != 200) {
 			return null;
-
-		ByteArrayOutputStream output = new ByteArrayOutputStream();
-		InputStream input = connection.getInputStream();
-		try {
-			byte[] buffer = new byte[BUFFER_SIZE];
-			int read = -1;
-			while ((read = input.read(buffer)) != -1)
-				output.write(buffer, 0, read);
-		} finally {
-			closeAndSwallowException(input);
-			closeAndSwallowException(output);
 		}
-		avatar = new Avatar(hash, System.currentTimeMillis(), output.toByteArray());
-		this.avatars.put(hash, avatar);
-		return avatar;
-	}
 
-	private void closeAndSwallowException(Closeable closable) {
-		try {
-			closable.close();
-		} catch (IOException e) {
+		try (
+			final InputStream inputStream = connection.getInputStream();
+			final ByteArrayOutputStream output = new ByteArrayOutputStream();
+		) {
+			// Use Java's built-in mechanism for transferring image data
+			inputStream.transferTo(output);
+			
+			long lastUpdated = System.currentTimeMillis();
+			avatar = new Avatar(hash, lastUpdated, output.toByteArray());
+			this.avatars.put(hash, avatar);
+			
+			return avatar;
 		}
 	}
 
@@ -267,20 +286,16 @@ public class AvatarStore implements Serializable, ISchedulingRule, IAvatarStore 
 	}
 
 	private String digest(String value) {
-		String hashed = null;
 		try {
-			byte[] digested = MessageDigest.getInstance(HASH_ALGORITHM).digest(value.getBytes(CHARSET));
-			hashed = new BigInteger(1, digested).toString(16);
-			int padding = HASH_LENGTH - hashed.length();
-			if (padding > 0) {
-				char[] zeros = new char[padding];
-				Arrays.fill(zeros, '0');
-				hashed = new String(zeros) + hashed;
-			}
+			MessageDigest algorithm = MessageDigest.getInstance(HASH_ALGORITHM);
+			byte[] input = value.getBytes(CHARSET);
+			byte[] digest = algorithm.digest(input);
+			
+			BigInteger digestNum = new BigInteger(1, digest);
+			return String.format(FORMAT_PATTERN, digestNum);
 		} catch (NoSuchAlgorithmException e) {
-			hashed = null;
+			return null;
 		}
-		return hashed;
 	}
 
 	/**
@@ -308,6 +323,11 @@ public class AvatarStore implements Serializable, ISchedulingRule, IAvatarStore 
 	 * @return hash
 	 */
 	public String getAdaptedHash(Object element) {
+		// Add early null check
+		if (element == null) {
+			return null;
+		}
+		
 		IAvatarHashProvider provider = null;
 		
 		if (element instanceof IAvatarHashProvider) {
@@ -318,15 +338,10 @@ public class AvatarStore implements Serializable, ISchedulingRule, IAvatarStore 
 		
 		if (provider != null) {
 			return provider.getAvatarHash();
+		}
 
-		//added by bbanfai to avoid NPEs
-		} else if (element == null) {
-			return null;
-		}
-		else {
-			String potentialHash = element.toString();
-			return isValidHash(potentialHash) ? potentialHash : getHash(potentialHash);
-		}
+		String potentialHash = element.toString();
+		return isValidHash(potentialHash) ? potentialHash : getHash(potentialHash);
 	}
 
 	/**
